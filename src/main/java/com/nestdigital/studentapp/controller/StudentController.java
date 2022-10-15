20ad73e5b85ef43908a1e3fae3efdcc7d5f9c706
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-15 16:46:28 +0530

    Update StudentController.java

--- src/main/java/com/nestdigital/studentapp/controller/StudentController.java
+++ src/main/java/com/nestdigital/studentapp/controller/StudentController.java
@@ -1,4 +1,18 @@
 package com.nestdigital.studentapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "<h1>You are in Home Page</h1>";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage(){
+        return "You are in contact page";
+    }
 }
